bf962e955841e4aab7ff4cff0d4adf3ffcd06f0e
Daniel Moore <wron2@hotmail.com>
2021-05-21 12:27:59 -0700

    Added express-sessions and established cookies

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
